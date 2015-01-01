1fad056881c55814a12088da11984c14af9076ca
noureldien <nreldien@yahoo.com>
2015-01-01 10:58:27 +0000

    Commit

---
+++ BlocksWorld/Model/Index.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocksWorld
+{
+    public struct Index
+    {
+        public int Y { get; set; }
+
+        public int X { get; set; }
+
+        public Index(int y, int x)
+            : this()
+        {
+            Y = y;
+            X = x;
+        }
+    }
+}
